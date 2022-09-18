a588a7d7a1a9b9ec5e4759e76c206f87156a80d3
Olena <pampam32121@gmail.com>
2022-09-18 20:42:03 +0300

    added state for holding text value in textarea, added sunction to calculate words in text

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import React from 'react';
 import './App.css';
 
 function App() {
+  
+  const [text, setText] = React.useState('');
+
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+
+  function calculateWordCount(text) {
+    const wordsArr = text.split(' ');
+    return wordsArr.filter(word => word !== '').length;
+  }
+
   return (
     <main>
-          <h1>How fast do you type?</h1>
-          <textarea />
-          <h4>Time remaining: ???</h4>
-          <button>START</button>
-          <h1>Word Count: ???</h1>
-      </main>
+      <h1>How fast do you type?</h1>
+      <textarea onChange={handleChange} value={text}/>
+      <h4>Time remaining: ???</h4>
+      <button onClick={() => calculateWordCount(text)}>START</button>
+      <h1>Word Count: ???</h1>
+    </main>
   );
-}
+};
 
 export default App;
