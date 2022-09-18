807080a91b9706f4b3ef414378f56f8181b35894
Olena <pampam32121@gmail.com>
2022-09-18 22:03:07 +0300

    added countdown after the start btn is pressed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
   
-  const [text, setText] = React.useState('');
+  const [text, setText] = useState('');
+  const [timeRemaining, setTimeRemaining] = useState(5);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining > 0) {
+        setTimeout(() => {
+            setTimeRemaining(time => time - 1)
+        }, 1000)
+    } else if (isTimeRunning && timeRemaining === 0) {
+      setIsTimeRunning(false)
+    }
+  }, [timeRemaining, isTimeRunning])
 
   function handleChange(e) {
     setText(e.target.value);
   }
 
-  function calculateWordCount(text) {
-    const wordsArr = text.split(' ');
-    return wordsArr.filter(word => word !== '').length;
+  // function calculateWordCount(text) {
+  //   const wordsArr = text.split(' ');
+  //   return wordsArr.filter(word => word !== '').length;
+  // }
+
+  function startCountdown() {
+    setIsTimeRunning(true)
   }
 
   return (
     <main>
       <h1>How fast do you type?</h1>
       <textarea onChange={handleChange} value={text}/>
-      <h4>Time remaining: ???</h4>
-      <button onClick={() => calculateWordCount(text)}>START</button>
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button onClick={startCountdown}>START</button>
       <h1>Word Count: ???</h1>
     </main>
   );
