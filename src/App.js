6a80c6c9a57688746be5c5c332a7b4e99c3466e8
Olena <pampam32121@gmail.com>
2022-09-22 23:49:04 +0300

    moved all business logic to custom hook

--- src/App.js
+++ src/App.js
@@ -1,49 +1,17 @@
-import React, {useState, useEffect, useRef} from 'react';
-import './App.css';
+import React from 'react';
+import useWordGame from './useWordGame';
 
 function App() {
   
-  const STARTING_TIME = 5;
-
-  const [text, setText] = useState('');
-  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
-  const [isTimeRunning, setIsTimeRunning] = useState(false);
-  const [wordCount, setWordCount] = useState(0);
-  const textBoxRef = useRef(null);
-
-  useEffect(() => {
-    if (isTimeRunning && timeRemaining > 0) {
-        setTimeout(() => {
-            setTimeRemaining(time => time - 1);
-        }, 1000)
-    } else if (isTimeRunning && timeRemaining === 0) {
-      endGame();
-    }
-  // eslint-disable-next-line
-  }, [timeRemaining, isTimeRunning]);
-
-  function handleChange(e) {
-    setText(e.target.value);
-  };
-
-  function calculateWordCount(text) {
-    const wordsArr = text.split(' ');
-    return wordsArr.filter(word => word !== '').length;
-  };
-
-  function startGame() {
-    setIsTimeRunning(true);
-    setText('');
-    setTimeRemaining(STARTING_TIME);
-    setWordCount(0);
-    textBoxRef.current.disabled = false;
-    textBoxRef.current.focus()
-  };
-
-  function endGame() {
-    setIsTimeRunning(false);
-    setWordCount(calculateWordCount(text));
-  }
+  const {
+    textBoxRef, 
+    handleChange, 
+    text, 
+    isTimeRunning, 
+    timeRemaining, 
+    startGame, 
+    wordCount
+  } = useWordGame(10);
 
   return (
     <main>
